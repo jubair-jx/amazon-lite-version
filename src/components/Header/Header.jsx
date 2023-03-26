56f9b6b90bae8dd59775c47567a3bbc20175d2ca
Mohammad Jubair <you@example.com>
2023-03-27 02:37:37 +0600

    Navbar Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is Header Area</h1>
+    <div className="header">
+      <img src={logo} alt="" />
+      <div className="menu">
+        <a href="/order">Order</a>
+        <a href="/orderreview">Order Review</a>
+        <a href="/manage">Manage Inventory</a>
+        <a href="/login">Login</a>
+      </div>
     </div>
   );
 };
