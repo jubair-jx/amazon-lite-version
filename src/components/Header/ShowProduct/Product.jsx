8f78915ed3d10cc9d5b9e7698ede187b502a0057
Mohammad Jubair <you@example.com>
2023-03-27 16:06:02 +0600

    Cart Done

--- src/components/Header/ShowProduct/Product.jsx
+++ src/components/Header/ShowProduct/Product.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import "./Product.css";
 const Product = (props) => {
   console.log(props);
-  const { price, img, name, rating, seller, category } = props.product;
+  const { price, img, name, ratings, seller, category } = props.product;
   return (
-    <div className="cart-items">
+    <div className="cart-item">
       <img src={img} alt="" />
+      <div className="cart-des">
+        <h2 className="cart-title">{name}</h2>
+        <p>Price : ${price}</p>
+        <p>Manufacturer : {seller}</p>
+        <p>Rating : {ratings} Star</p>
+      </div>
+      <button className="btn">Add to Cart</button>
     </div>
   );
 };
