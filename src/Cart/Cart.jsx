65d23aef618b37cdb3b81b69cf25889e6ce03502
Mohammad Jubair <you@example.com>
2023-03-28 10:15:39 +0600

    Cart is Working Here

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -4,18 +4,22 @@ import "./Cart.css";
 const Cart = (props) => {
   const { cart } = props;
   console.log(cart);
-  let total = 0;
+  let totalPrice = 0;
+  let totalShipping = 0;
   for (const product of cart) {
-    total = total + product.price;
+    totalPrice = totalPrice + product.price;
+    totalShipping += product.shipping;
   }
+  let tax = (totalPrice * 7) / 100;
+  let grandTotal = totalPrice + tax + totalShipping;
   return (
     <div className="carts">
       <h3>Order Summary : </h3>
       <p>Selected Items : {cart.length}</p>
-      <p>Total Price : $ {total}</p>
-      <p>Total Shipping Charge : $</p>
-      <p>Tax : </p>
-      <h6>Grand Total : $ </h6>
+      <p>Total Price : $ {totalPrice}</p>
+      <p>Total Shipping Charge : $ {totalShipping}</p>
+      <p>Tax : {tax.toFixed(2)}</p>
+      <h6>Grand Total : ${grandTotal.toFixed(2)} </h6>
     </div>
   );
 };
